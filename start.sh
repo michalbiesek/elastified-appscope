b8e95c48891e3b96f94b5eae7908691e18daa12b
michalbiesek <mbiesek@cribl.io>
2022-02-25 18:22:13 +0100

    Export kibana configuration file to .env file

--- start.sh
+++ start.sh
@@ -37,7 +37,7 @@ done
 
 printf '\n'
 echo "Copying the kibana Configuration"
-curl -X POST http://localhost:$KIBANA_HOST_PORT/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@elastic_cfg.ndjson
+curl -X POST http://localhost:$KIBANA_HOST_PORT/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@$KIBANA_CFG_FILE
 
 printf '\n'
 echo "Demo is ready."
