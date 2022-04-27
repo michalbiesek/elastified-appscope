c8ef01838a1469ce09311148b7eafb475cd37ef5
michalbiesek <mbiesek@cribl.io>
2022-04-27 16:23:12 +0200

    Add seperate container for TLS communication
    - `appscope01_tls` can be used to scope individual commands and use
      TLS destination path
    - use TCP source destination with TLS enabled

--- start.sh
+++ start.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 source .env
 
-
+KEY_CERT_DIR="/opt/"
+PRIVATE_KEY_NAME="domain.key"
+CERT_NAME="domain.crt"
 VM_COUNT=`sysctl -n vm.max_map_count`
+
+#######################################
+# Copy private key to specific docker container
+# Arguments:
+#   Name of the docker container
+#######################################
+copy_key () {
+    local output_dir=$1:$KEY_CERT_DIR
+
+    echo "Copying the TLS private key to $output_dir"
+    docker cp $PRIVATE_KEY_NAME $output_dir$PRIVATE_KEY_NAME
+}
+
+#######################################
+# Copy certificate to specific docker container
+# Arguments:
+#   Name of the docker container
+#######################################
+copy_cert () {
+    local output_dir=$1:$KEY_CERT_DIR
+
+    echo "Copying the TLS certificate to $output_dir"
+    docker cp $CERT_NAME $output_dir$CERT_NAME
+}
 
 echo "Checking virtual memory settings"
 
@@ -17,6 +43,13 @@ docker-compose --env-file .env up -d --build
 
 echo "Copying the Cribl Configuration"
 docker cp cribl/ cribl01:/opt/cribl/local/
+
+echo "Generate TLS keys"
+openssl req -newkey rsa:2048 -nodes -keyout $PRIVATE_KEY_NAME -x509 -days 365 -out $CERT_NAME -subj "/C=PL/ST=Warsaw/L=GoatTown/O=Cribl/OU=Appscope/CN=cribl.io"
+
+copy_key "cribl01"
+copy_cert "cribl01"
+copy_cert "appscope01_tls"
 
 echo "Waiting for the kibana to start"
 
@@ -41,5 +74,8 @@ curl -X POST http://localhost:$KIBANA_HOST_PORT/api/saved_objects/_import?overwr
 
 printf '\n'
 echo "Demo is ready."
+echo "To use TCP:"
 echo "To start scoping bash session run: docker-compose run appscope01"
 echo "To start scoping individual commands run: docker-compose run appscope02 and use ldscope/scope"
+echo "To use TLS:"
+echo "To start scoping individual commands run: docker exec -it appscope01_tls bash and use ldscope/scope"
--- stop.sh
+++ stop.sh
@@ -2,3 +2,5 @@
 
 echo "Stop docker compose"
 docker-compose down
+rm domain.crt 2> /dev/null
+rm domain.key 2> /dev/null
