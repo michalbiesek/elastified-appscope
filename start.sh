d8f2ee80bd362222ce7fbb81cbe0b9912eed48f5
michalbiesek <mbiesek@cribl.io>
2022-06-15 11:42:44 +0200

    [WIP] SSHD scoping

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ source .env
 KEY_CERT_DIR="/opt/"
 PRIVATE_KEY_NAME="domain.key"
 CERT_NAME="domain.crt"
-VM_COUNT=`sysctl -n vm.max_map_count`
 
 #######################################
 # Copy private key to specific docker container
@@ -30,13 +29,8 @@ copy_cert () {
     docker cp $CERT_NAME $output_dir$CERT_NAME
 }
 
-echo "Checking virtual memory settings"
-
-if [ $VM_COUNT -lt $VM_EXPECTED_LIMIT ];then
-    echo "Error with vm.max_map_count settings value $VM_COUNT it too low"
-    echo "Please change the limit with: 'sudo sysctl -w vm.max_map_count=$VM_EXPECTED_LIMIT'"
-    exit 1
-fi
+echo "Copying public key(s) from host to docker container"
+cp ~/.ssh/*pub images/
 
 echo "Start docker compose"
 docker-compose --env-file .env up -d --build
@@ -50,27 +44,6 @@ openssl req -newkey rsa:2048 -nodes -keyout $PRIVATE_KEY_NAME -x509 -days 365 -o
 copy_key "cribl01"
 copy_cert "cribl01"
 copy_cert "appscope01_tls"
-
-echo "Waiting for the kibana to start"
-
-counter=0
-limit=10
-
-until $(curl --output /dev/null --silent --head --fail http://localhost:$KIBANA_HOST_PORT); do
-    if [ ${counter} -eq ${limit} ];then
-      printf '\n'
-      echo "Max attempts reached"
-      exit 1
-    fi
-
-    printf '.'
-    counter=$(($counter+1))
-    sleep 5
-done
-
-printf '\n'
-echo "Copying the kibana Configuration"
-curl -X POST http://localhost:$KIBANA_HOST_PORT/api/saved_objects/_import?overwrite=true -H "kbn-xsrf: true" --form file=@$KIBANA_CFG_FILE
 
 printf '\n'
 echo "Demo is ready."
